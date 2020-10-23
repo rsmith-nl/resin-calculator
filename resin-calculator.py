5b6b9b3f154527d59ec46d06972e01c66f3aab33
Roland Smith <rsmith@xs4all.nl>
2020-10-23 21:35:14 +0200

    Reformat with black.

--- resin-calculator.py
+++ resin-calculator.py
@@ -30,23 +30,23 @@ def pround(val):
         precision = 0
     if val < 1:
         precision = 3
-    return f'{val:.{precision}f}'
+    return f"{val:.{precision}f}"
 
 
 def load_data():
     """Load the resin data."""
     try:
-        with open(os.environ['HOME'] + os.sep + 'resins.json') as rf:
+        with open(os.environ["HOME"] + os.sep + "resins.json") as rf:
             lines = rf.readlines()
     except (FileNotFoundError, KeyError):
-        with open('resins.json') as rf:
+        with open("resins.json") as rf:
             lines = rf.readlines()
-    text = '\n'.join([ln.strip() for ln in lines])
+    text = "\n".join([ln.strip() for ln in lines])
     try:
-        lm = re.search('// Last modified: (.*)', text).groups()[0]
+        lm = re.search("// Last modified: (.*)", text).groups()[0]
     except AttributeError:
         lm = None
-    nocomments = re.sub('^//.*$', '', text, flags=re.MULTILINE)
+    nocomments = re.sub("^//.*$", "", text, flags=re.MULTILINE)
     return loads(nocomments), lm
 
 
@@ -56,63 +56,63 @@ def create_widgets(root):
     default_font.configure(size=12)
     root.option_add("*Font", default_font)
     # General commands and bindings
-    root.bind_all('q', do_exit)
+    root.bind_all("q", do_exit)
     # Make selected rows and columns resizable
     root.columnconfigure(2, weight=1)
     root.rowconfigure(2, weight=1)
     # Create widgets.
-    ttk.Label(
-        root, text="Resin recipe:", anchor='e'
-    ).grid(
-        row=0, column=0, columnspan=2, sticky='we'
+    ttk.Label(root, text="Resin recipe:", anchor="e").grid(
+        row=0, column=0, columnspan=2, sticky="we"
     )
     resinchoice = ttk.Combobox(
-        root, values=keys, state='readonly', justify='right', width=26
+        root, values=keys, state="readonly", justify="right", width=26
     )
-    resinchoice.grid(row=0, column=2, columnspan=2, sticky='ew')
+    resinchoice.grid(row=0, column=2, columnspan=2, sticky="ew")
     resinchoice.bind("<<ComboboxSelected>>", on_resintype)
-    qt = ('Total quantity:', 'First component quantity:')
+    qt = ("Total quantity:", "First component quantity:")
     quantitytype = ttk.Combobox(
-        root, values=qt, state='readonly', justify='right', width=22
+        root, values=qt, state="readonly", justify="right", width=22
     )
     quantitytype.current(0)
-    quantitytype.grid(row=1, column=0, columnspan=2, sticky='w')
+    quantitytype.grid(row=1, column=0, columnspan=2, sticky="w")
     quantitytype.bind("<<ComboboxSelected>>", on_quantitytype)
-    qedit = ttk.Entry(root, justify='right')
-    qedit.insert(0, '100')
-    qedit.grid(row=1, column=2, sticky='ew')
+    qedit = ttk.Entry(root, justify="right")
+    qedit.insert(0, "100")
+    qedit.grid(row=1, column=2, sticky="ew")
     vcmd = root.register(is_number)
-    qedit['validate'] = 'key'
-    qedit['validatecommand'] = (vcmd, '%P')
-    ttk.Label(root, text='g').grid(row=1, column=3, sticky='w')
+    qedit["validate"] = "key"
+    qedit["validatecommand"] = (vcmd, "%P")
+    ttk.Label(root, text="g").grid(row=1, column=3, sticky="w")
     result = ttk.Treeview(
-        root, columns=('component', 'quantity', 'unit', 'ape'), selectmode="none",
+        root,
+        columns=("component", "quantity", "unit", "ape"),
+        selectmode="none",
     )
     style = ttk.Style(root)
-    style.configure('Treeview', rowheight=24)
-    result.heading('component', text='Component', anchor='w')
-    result.heading('quantity', text='Quantity', anchor='e')
-    result.heading('unit', text='Unit', anchor='w')
-    result.heading('ape', text='1/kg', anchor='e')
-    result.column('#0', width='0', stretch=False)
-    result.column('component', anchor='w')
-    result.column('quantity', anchor='e', stretch=False, width=100)
-    result.column('unit', anchor='w', stretch=False, width=40)
-    result.column('ape', anchor='e', stretch=False, width=80)
-    result.grid(row=2, column=0, columnspan=4, sticky='nesw')
-    result.bind('<<UpdateNeeded>>', do_update)
+    style.configure("Treeview", rowheight=24)
+    result.heading("component", text="Component", anchor="w")
+    result.heading("quantity", text="Quantity", anchor="e")
+    result.heading("unit", text="Unit", anchor="w")
+    result.heading("ape", text="1/kg", anchor="e")
+    result.column("#0", width="0", stretch=False)
+    result.column("component", anchor="w")
+    result.column("quantity", anchor="e", stretch=False, width=100)
+    result.column("unit", anchor="w", stretch=False, width=40)
+    result.column("ape", anchor="e", stretch=False, width=80)
+    result.grid(row=2, column=0, columnspan=4, sticky="nesw")
+    result.bind("<<UpdateNeeded>>", do_update)
     prbut = ttk.Button(root, text="Print recipe", command=do_print)
-    prbut.grid(row=3, column=0, sticky='w')
+    prbut.grid(row=3, column=0, sticky="w")
     savebut = ttk.Button(root, text="Save recipe", command=do_saveas)
-    savebut.grid(row=3, column=1, sticky='w')
+    savebut.grid(row=3, column=1, sticky="w")
     if state.filedate:
         dflabel = ttk.Label(
             root,
-            text='Data file modification date: ' + state.filedate,
-            anchor='center',
-            foreground='#777777'
+            text="Data file modification date: " + state.filedate,
+            anchor="center",
+            foreground="#777777",
         )
-        dflabel.grid(row=4, column=1, columnspan=4, sticky='ew')
+        dflabel.grid(row=4, column=1, columnspan=4, sticky="ew")
     resinchoice.focus_set()
     # Return the widgets that are referenced by other functions.
     w = SimpleNamespace(
@@ -123,8 +123,8 @@ def create_widgets(root):
 
 def is_number(data):
     """Validate the contents of an entry widget as a float."""
-    if data == '':
-        w.result.event_generate('<<UpdateNeeded>>', when='tail')
+    if data == "":
+        w.result.event_generate("<<UpdateNeeded>>", when="tail")
         return True
     try:
         rv = float(data)
@@ -132,7 +132,7 @@ def is_number(data):
             return False
     except ValueError:
         return False
-    w.result.event_generate('<<UpdateNeeded>>', when='tail')
+    w.result.event_generate("<<UpdateNeeded>>", when="tail")
     return True
 
 
@@ -152,7 +152,7 @@ def on_resintype(event):
     val = w.resinchoice.get()
     text2 = w.qedit.get()
     if val and text2:
-        w.result.event_generate('<<UpdateNeeded>>', when='tail')
+        w.result.event_generate("<<UpdateNeeded>>", when="tail")
     w.qedit.focus_set()
 
 
@@ -162,7 +162,7 @@ def on_quantitytype(event):
     val = w.quantitytype.current()
     if val != state.qtype:
         state.qtype = val
-        w.result.event_generate('<<UpdateNeeded>>', when='tail')
+        w.result.event_generate("<<UpdateNeeded>>", when="tail")
 
 
 def get_amount():
@@ -198,15 +198,15 @@ def do_update(event):
         w.result.delete(item)
     if quant > 0:
         state.current_recipe = tuple(
-            (name, pround(c * factor), f'{int(100000/(c*factor))/100:.2f}')
+            (name, pround(c * factor), f"{int(100000/(c*factor))/100:.2f}")
             for name, c in components
         )
     else:
         state.current_recipe = tuple((name, 0, 0) for name, c in components)
     for name, amount, ape in state.current_recipe:
-        w.result.insert("", 'end', values=(name, amount, 'g', ape))
+        w.result.insert("", "end", values=(name, amount, "g", ape))
     q = sum(float(amnt) for _, amnt, _ in state.current_recipe)
-    w.result.insert("", 'end', values=('total:', pround(q), 'g', ''))
+    w.result.insert("", "end", values=("total:", pround(q), "g", ""))
 
 
 def make_text(state):
@@ -219,23 +219,20 @@ def make_text(state):
     amlen = max((amlen, len(pround(q))))
     apulen = max(len(apu) for _, _, apu in state.current_recipe)
     lines = [
-        f'Resin calculator v{__version__}',
-        '------------------------',
-        '',
-        f'Recipe for: {state.current_name}',
-        f'Date: {str(datetime.now())[:-7]}',
-        'User: {state.uname}',
-        ''
+        f"Resin calculator v{__version__}",
+        "------------------------",
+        "",
+        f"Recipe for: {state.current_name}",
+        f"Date: {str(datetime.now())[:-7]}",
+        "User: {state.uname}",
+        "",
     ]
     lines += [
-        f'{name:{namelen}s}: {amount:>{amlen}} g ({apu:>{apulen}} /kg)'
+        f"{name:{namelen}s}: {amount:>{amlen}} g ({apu:>{apulen}} /kg)"
         for name, amount, apu in state.current_recipe
     ]
-    lines += [
-        '-' * (namelen + 4 + amlen),
-        f'{"":{namelen + 2}s}{pround(q):>{amlen}} g'
-    ]
-    return '\n'.join(lines)
+    lines += ["-" * (namelen + 4 + amlen), f'{"":{namelen + 2}s}{pround(q):>{amlen}} g']
+    return "\n".join(lines)
 
 
 def do_print(event):
@@ -243,8 +240,8 @@ def do_print(event):
     if state.current_recipe is None:
         return
     text = make_text(state.current_recipe, state.current_name, state)
-    filename = 'resin-calculator-output.txt'
-    with open(filename, 'w') as pf:
+    filename = "resin-calculator-output.txt"
+    with open(filename, "w") as pf:
         pf.write(text)
     printfile(filename)
 
@@ -255,52 +252,54 @@ def do_saveas():
         return
     fn = filedialog.asksaveasfilename(
         parent=root,
-        defaultextension='.txt',
-        filetypes=(('text files', '*.txt'), ('all files', '*.*')),
+        defaultextension=".txt",
+        filetypes=(("text files", "*.txt"), ("all files", "*.*")),
         initialfile=state.current_name,
-        initialdir=os.environ['HOME']
+        initialdir=os.environ["HOME"],
     )
     if not len(fn):
         return
     text = make_text(state)
-    with open(fn, 'w') as pf:
+    with open(fn, "w") as pf:
         pf.write(text)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Platform specific set-up
-    if os.name == 'nt':
+    if os.name == "nt":
         from win32api import ShellExecute
         from win32print import GetDefaultPrinter
-        uname = os.environ['USERNAME']
+
+        uname = os.environ["USERNAME"]
 
         def printfile(fn):
             """Print the given file using the default printer."""
             dp = GetDefaultPrinter()
-            rv = ShellExecute(0, 'print', fn, f'/d: "{dp}"', '.', 0)
+            rv = ShellExecute(0, "print", fn, f'/d: "{dp}"', ".", 0)
             if 0 < rv <= 32:
-                messagebox.showerror('Printing failed', f'Error code: {rv}')
+                messagebox.showerror("Printing failed", f"Error code: {rv}")
 
-    elif os.name == 'posix':
+    elif os.name == "posix":
         from subprocess import run
-        uname = os.environ['USER']
+
+        uname = os.environ["USER"]
 
         def printfile(fn):
             """Print the given file using “lpr”."""
-            cp = run(['lpr', fn])
+            cp = run(["lpr", fn])
             if cp.returncode != 0:
-                messagebox.showerror('Printing failed', f'Error code: {cp.returncode}')
+                messagebox.showerror("Printing failed", f"Error code: {cp.returncode}")
 
         # Detach from terminal.
         if os.fork():
             sys_exit()
 
     else:
-        uname = 'unknown'
+        uname = "unknown"
 
         def printfile(fn):
             """Report that printing is not supported."""
-            messagebox.showinfo('Printing', 'Printing is not supported on this OS.')
+            messagebox.showinfo("Printing", "Printing is not supported on this OS.")
 
     # Global data
     state = SimpleNamespace()
@@ -308,10 +307,10 @@ if __name__ == '__main__':
     state.qtype = 0
     state.quantity = 0
     state.current_recipe = None
-    state.current_name = ''
+    state.current_name = ""
     state.uname = uname
     # Create and run the GUI.
     root = tk.Tk(None)
     w = create_widgets(root)
-    root.wm_title('Resin calculator v' + __version__)
+    root.wm_title("Resin calculator v" + __version__)
     root.mainloop()
